e74b60e975191d5a525bd2c06eebcd8293d3abcb
Silas Stoffel <silasstofel@gmail.com>
2021-09-27 23:53:43 -0300

    fix: formatter

--- src/modules/accounts/useCases/AuthenticateUser/AuthenticateUserUseCase.ts
+++ src/modules/accounts/useCases/AuthenticateUser/AuthenticateUserUseCase.ts
@@ -1,62 +1,63 @@
-import { container, inject, injectable } from "tsyringe";
+import { inject, injectable } from "tsyringe";
 import { compare } from "bcrypt";
 import { sign } from "jsonwebtoken";
 import { IUsersRepository } from "../../repositories/IUsersRepository";
+import { AppError } from "../../../../Erros/AppError";
 
 interface IRequest {
-  email: string;
-  password: string;
+    email: string;
+    password: string;
 }
 
 interface IResponse {
-  user: {
-    email: string;
-    name: string;
-  };
-  token: string;
+    user: {
+        email: string;
+        name: string;
+    };
+    token: string;
 }
 
 @injectable()
 export class AuthenticateUserUseCase {
-  constructor(
-    @inject("UsersRepository")
-    private usersRepository: IUsersRepository
-  ) {}
+    constructor(
+        @inject("UsersRepository")
+        private usersRepository: IUsersRepository
+    ) {}
 
-  async execute({ email, password }: IRequest): Promise<IResponse> {
-    const user = await this.usersRepository.findByEmail(email);
+    async execute({ email, password }: IRequest): Promise<IResponse> {
+        const user = await this.usersRepository.findByEmail(email);
 
-    if (!user) {
-    throw new Error("E-mail or password invalid.");
+        if (!user) {
+            throw new AppError("E-mail or password invalid.");
+        }
+
+        const validPassword = await compare(password, user.password);
+        if (!validPassword) {
+            throw new AppError("E-mail or password invalid.");
+        }
+
+        // Token.Silas.Curso.Ignite <https://argon2.online/>
+        const secret =
+            "$argon2id$v=19$m=16,t=2,p=1$MVBVQ3pGWjBWcURnUzNKQg$aY/p2v/MkTikOJoIsP1dWQ";
+        const config = {
+            subject: user.id,
+            expiresIn: "1d",
+        };
+
+        const payload = {
+            email: user.email,
+            name: user.email,
+        };
+
+        const token = sign(payload, secret, config);
+        const authResponse: IResponse = {
+            token,
+            user: {
+                name: user.name,
+                email: user.email,
+            },
+        };
+
+        return authResponse;
     }
-
-    const validPassword = await compare(password, user.password);
-    if (!validPassword) {
-    throw new Error("E-mail or password invalid.");
-    }
-
-    // Token.Silas.Curso.Ignite <https://argon2.online/>
-    const secret =
-    "$argon2id$v=19$m=16,t=2,p=1$MVBVQ3pGWjBWcURnUzNKQg$aY/p2v/MkTikOJoIsP1dWQ";
-    const config = {
-    subject: user.id,
-    expiresIn: "1d",
-    };
-
-    const payload = {
-    email: user.email,
-    name: user.email,
-    };
-
-    const token = sign(payload, secret, config);
-    const authResponse: IResponse = {
-    token,
-    user: {
-        name: user.name,
-        email: user.email,
-    },
-    };
-
-    return authResponse;
-  }
 }

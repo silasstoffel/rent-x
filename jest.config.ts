f50a9d85b6214f25ed31ddfbc63a6f0387dcf5d5
Silas Stoffel <silasstofel@gmail.com>
2021-11-20 14:51:39 -0300

    chore: enable coverage

--- jest.config.ts
+++ jest.config.ts
@@ -1,18 +1,20 @@
-import { pathsToModuleNameMapper } from "ts-jest/utils";
-import { compilerOptions } from "./tsconfig.json";
+import {pathsToModuleNameMapper} from "ts-jest/utils";
+import {compilerOptions} from "./tsconfig.json";
 
 export default {
     bail: true,
     clearMocks: true,
 
     // Indicates whether the coverage information should be collected while executing the test
-    // collectCoverage: false,
+    collectCoverage: true,
 
     // An array of glob patterns indicating a set of files for which coverage information should be collected
-    // collectCoverageFrom: undefined,
+    collectCoverageFrom: [
+        "<rootDir>/src/modules/**/useCases/**/*.ts"
+    ],
 
     // The directory where Jest should output its coverage files
-    // coverageDirectory: undefined,
+    coverageDirectory: "coverage",
 
     // An array of regexp pattern strings used to skip coverage collection
     // coveragePathIgnorePatterns: [
@@ -23,12 +25,13 @@ export default {
     coverageProvider: "v8",
 
     // A list of reporter names that Jest uses when writing coverage reports
-    // coverageReporters: [
-    //   "json",
-    //   "text",
-    //   "lcov",
-    //   "clover"
-    // ],
+    coverageReporters: [
+        "text-summary",
+        //   "json",
+        //   "text",
+        "lcov",
+        //   "clover"
+    ],
 
     // An object that configures minimum threshold enforcement for coverage results
     // coverageThreshold: undefined,

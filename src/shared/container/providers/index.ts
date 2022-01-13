395bde354110f6ee62b4094ad9833fbb163167b8
Silas Stoffel <silasstofel@gmail.com>
2022-01-12 22:03:01 -0300

    feat: created email provider (AWS SES)

--- src/shared/container/providers/index.ts
+++ src/shared/container/providers/index.ts
@@ -1,16 +1,32 @@
-import {container} from "tsyringe";
+import { container } from "tsyringe";
 
-import {IDateProvider} from "@shared/container/providers/date/IDateProvider";
-import {DayjsDateProvider} from "@shared/container/providers/date/implementations/DayjsDateProvider";
-import {EtherealMailProvider} from "@shared/container/providers/mail/implementations/EtherealMailProvider";
-import {IMailProvider} from "@shared/container/providers/mail/IMailProvider";
-import {IStorageProvider} from "@shared/container/providers/storage/IStorageProvider";
-import {LocalStorageProvider} from "@shared/container/providers/storage/implementations/LocalStorageProvider";
-import {S3StorageProvider} from "@shared/container/providers/storage/implementations/S3StorageProvider";
+import { IDateProvider } from "@shared/container/providers/date/IDateProvider";
+import { DayjsDateProvider } from "@shared/container/providers/date/implementations/DayjsDateProvider";
+import { EtherealMailProvider } from "@shared/container/providers/mail/implementations/EtherealMailProvider";
+import { SESMailProvider } from "@shared/container/providers/mail/implementations/SESMailProvider";
+import { IMailProvider } from "@shared/container/providers/mail/IMailProvider";
+import { IStorageProvider } from "@shared/container/providers/storage/IStorageProvider";
+import { LocalStorageProvider } from "@shared/container/providers/storage/implementations/LocalStorageProvider";
+import { S3StorageProvider } from "@shared/container/providers/storage/implementations/S3StorageProvider";
 
-container.registerSingleton<IDateProvider>("DayjsDateProvider", DayjsDateProvider);
-container.registerInstance<IMailProvider>("EtherealMailProvider", new EtherealMailProvider());
+const { MAIL_PROVIDER, STORAGE_DRIVER } = process.env;
 
-const storageDriver = process.env.STORAGE_DRIVER || 'local';
-const storage = storageDriver === 's3' ? new S3StorageProvider() : new LocalStorageProvider();
+container.registerSingleton<IDateProvider>(
+    "DayjsDateProvider",
+    DayjsDateProvider
+);
+
+const mailProvider =
+    MAIL_PROVIDER === "ses"
+        ? new SESMailProvider()
+        : new EtherealMailProvider();
+
+container.registerInstance<IMailProvider>("MailProvider", mailProvider);
+
+const storageDriver = STORAGE_DRIVER || "local";
+const storage =
+    storageDriver === "s3"
+        ? new S3StorageProvider()
+        : new LocalStorageProvider();
+
 container.registerInstance<IStorageProvider>("StorageProvider", storage);

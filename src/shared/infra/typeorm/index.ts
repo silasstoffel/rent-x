31437d19f4058e14baea066cb37aaba841264cf2
Silas Stoffel <silasstofel@gmail.com>
2022-01-16 18:25:19 -0300

    chore: default hostname

--- src/shared/infra/typeorm/index.ts
+++ src/shared/infra/typeorm/index.ts
@@ -1,6 +1,6 @@
 import { Connection, createConnection, getConnectionOptions } from "typeorm";
 
-export default async (host: string = "database"): Promise<Connection> => {
+export default async (host: string = "localhost"): Promise<Connection> => {
     const defaultOptions = await getConnectionOptions();
     const database = process.env.NODE_ENV === 'test' ? 'rentx_test' : defaultOptions.database;
     return createConnection(Object.assign(defaultOptions, { host, database }));

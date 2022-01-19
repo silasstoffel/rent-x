d644621475cf6d4936dc71b8ed45702ac4e4648b
Silas Stoffel <silasstofel@gmail.com>
2022-01-18 21:50:28 -0300

    chore: enabled cors

--- src/shared/infra/http/app.ts
+++ src/shared/infra/http/app.ts
@@ -4,6 +4,7 @@ createConnection();
 import express, {NextFunction, Request, Response} from "express";
 import "express-async-errors";
 import swaggerUi from "swagger-ui-express";
+import cors from "cors";
 
 import swaggerFile from "../../../swagger.json";
 import "../../container";
@@ -13,7 +14,10 @@ import upload from "@config/upload";
 
 
 const app = express();
+
 app.use(express.json());
+app.use(cors());
+
 app.use("/api-docs", swaggerUi.serve, swaggerUi.setup(swaggerFile));
 app.use("/avatar", express.static(`${upload.tmpFolder}/avatar`));
 app.use("/avatar", express.static(`${upload.tmpFolder}/cars`));

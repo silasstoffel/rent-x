252502a231235ac1fd91dbf61e055b5baa14f3d9
Silas Stoffel <silasstofel@gmail.com>
2022-01-20 22:50:37 -0300

    refactor: redis conect

--- src/shared/infra/http/middleware/rateLimiter.ts
+++ src/shared/infra/http/middleware/rateLimiter.ts
@@ -8,16 +8,9 @@ const redisClient = redis.createClient({
     socket: {
         host: process.env.REDIS_HOST,
         port: Number(process.env.REDIS_PORT),
-        sessionTimeout: 20,
     },
 });
 
-const limiter = new RateLimiterRedis({
-    storeClient: redisClient,
-    keyPrefix: "rateLimiter",
-    points: 6, // 10 requests
-    duration: 1, // per 1 second by IP
-});
 
 export default async function (
     request: Request,
@@ -26,11 +19,18 @@ export default async function (
 ): Promise<void> {
     try {
         await redisClient.connect();
+
+        const limiter = new RateLimiterRedis({
+            storeClient: redisClient,
+            keyPrefix: "rateLimiter",
+            points: 6, // 10 requests
+            duration: 1, // per 1 second by IP
+        });
+
         await limiter.consume(request.ip);
+
         return next();
     } catch (err) {
         throw new AppError("Too many request", 429);
-    } finally {
-        //await redisClient.disconnect();
     }
 }

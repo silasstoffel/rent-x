d2884c12c3b485e05590cc6c783cd70d06c26e21
Silas Stoffel <silasstofel@gmail.com>
2022-01-20 23:00:45 -0300

    refactor: redis connection

--- src/shared/infra/http/middleware/rateLimiter.ts
+++ src/shared/infra/http/middleware/rateLimiter.ts
@@ -3,23 +3,28 @@ import { RateLimiterRedis } from "rate-limiter-flexible";
 import { NextFunction, Request, Response } from "express";
 import { AppError } from "@shared/errors/AppError";
 
-const redisClient = redis.createClient({
-    legacyMode: true,
-    socket: {
-        host: process.env.REDIS_HOST,
-        port: Number(process.env.REDIS_PORT),
-    },
-});
-
-
 export default async function (
     request: Request,
     response: Response,
     next: NextFunction
 ): Promise<void> {
-    try {
-        await redisClient.connect();
+    let redisClient = null;
 
+    try {
+        redisClient = redis.createClient({
+            legacyMode: true,
+            socket: {
+                host: process.env.REDIS_HOST,
+                port: Number(process.env.REDIS_PORT),
+            },
+        });
+
+        await redisClient.connect();
+    } catch (err) {
+        throw new Error(`Redis error connection. ${err.message}`);
+    }
+
+    try {
         const limiter = new RateLimiterRedis({
             storeClient: redisClient,
             keyPrefix: "rateLimiter",
